8e359ad2ee5f267d830e6ba703c182b23b052513
Mahdi <mahdievilp311@gmail.com>
2025-09-02 12:22:35 +0330

    call api for random episode

---
+++ Rick-Morty/src/api/getEpisods.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+export default async function getEpisods () {
+  try {
+    const ep = []
+    for (let i = 0; i <= 3; i++) {
+      ep.push(Math.ceil(Math.random() * 28))
+    }
+    const res = await axios.get(`https://rickandmortyapi.com/api/episode/${ep}`)
+    return res.data.map(({ id, name, episode }) => ({
+      id: id,
+      name: name,
+      episode: episode
+    }))
+  } catch (e) {
+    console.log(e.error.massage)
+  }
+}
