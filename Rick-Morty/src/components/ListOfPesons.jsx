c2a865aa521c374ab01fea51146919f87b85d421
Mahdi <mahdievilp311@gmail.com>
2025-09-02 10:00:54 +0330

    add h1 to listOfPesons

--- Rick-Morty/src/components/ListOfPesons.jsx
+++ Rick-Morty/src/components/ListOfPesons.jsx
@@ -3,16 +3,18 @@ export default function ListOfPersons({person}){
     return(
         
         <>
-        <div className="grid grid-cols-4  w-[1500px] m-auto pt-[100px] pb-[70px]">
-            {person&& (
-                person.map((pr,prindex)=>{
-                    return(
-                        <Person key={prindex} pr={pr} />
-                    )
-                })
-            )}
+        <div className="relative">
+         <h1 className="text-[#FFFFFF] text-4xl font-bold absolute top-8 left-55  ">persons</h1>
+         <div className="grid grid-cols-4  w-[1500px] m-auto pt-[100px] pb-[70px]">
+             {person&& (
+                   person.map((pr,prindex)=>{
+                        return(
+                          <Person key={prindex} pr={pr} />
+                        )
+                   })
+              )}
+         </div>
         </div>
-
         </>
     )
 }
