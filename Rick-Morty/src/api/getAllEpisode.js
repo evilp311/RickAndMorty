f93d01966e58cbc501f78b05a14002cb7819ed8d
Mahdi <mahdievilp311@gmail.com>
2025-09-07 07:35:29 +0330

    fetch api for all episodes

--- Rick-Morty/src/api/getAllEpisode.js
+++ Rick-Morty/src/api/getAllEpisode.js
@@ -2,7 +2,11 @@ import axios from 'axios'
 
 export async function getAllEpisodes () {
   try {
-    const res = await axios.get(`https://rickandmortyapi.com/api/episode`)
+    const ep = []
+    for (let i = 1; i <= 51; i++) {
+      ep.push(i)
+    }
+    const res = await axios.get(`https://rickandmortyapi.com/api/episode/${ep}`)
     return res.data.results.map(({ id, name, episode }) => ({
       id: id,
       name: name,
